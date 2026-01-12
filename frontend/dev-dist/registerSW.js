da5df24b964c21c8b789f60054ebbc069e175f31
Dylserker <levant.dylan@gmail.com>
2026-01-12 14:30:40 +0100

    je sais pas ce que je fais du moment que sa marche mdr

--- frontend/dev-dist/registerSW.js
+++ frontend/dev-dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) navigator.serviceWorker.register('/dev-sw.js?dev-sw', { scope: '/', type: 'classic' })
+if('serviceWorker' in navigator) navigator.serviceWorker.register('/FocusTask/dev-sw.js?dev-sw', { scope: '/FocusTask/', type: 'classic' })
--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,10 +79,10 @@ define(['./workbox-52f2a342'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "registerSW.js",
-    "revision": "3ca0b8505b4bec776b69afdba2768812"
+    "revision": "b80ca3b5e3316d7d947bd1583ae7c98e"
   }, {
     "url": "index.html",
-    "revision": "0.uckptpf5254"
+    "revision": "0.ipo6plncnp8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
