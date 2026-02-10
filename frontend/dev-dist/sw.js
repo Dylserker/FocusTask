028798a7a0ab1ed3e5612a02a30fb216e590c963
Dylserker <levant.dylan@gmail.com>
2026-02-10 17:58:37 +0100

    Dark mode ajustement

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-52f2a342'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3m4np3d4uqo"
+    "revision": "0.qqjagk48rek"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
